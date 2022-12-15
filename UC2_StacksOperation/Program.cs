4fd5b08574f029e15d544d015e21b6795c66a710
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 21:45:56 +0530

    [yogita] peak and pop operation

---
+++ UC2_StacksOperation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using UC2_StacksOperation;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+
+        StacksUsingLinkedList obj = new StacksUsingLinkedList();
+
+        obj.push(70);
+        obj.push(30);
+        obj.push(56);
+        obj.display();
+        Console.Write("\nTop of the stacks {0}\n", obj.peek());
+    }
+}
---
+++ UC2_StacksOperation/StacksUsingLinkedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_StacksOperation
+{
+    public class StacksUsingLinkedList
+    {
+        private class Node
+        {
+            public int data;
+            public Node link;
+        }
+
+        Node top;
+        public StacksUsingLinkedList()
+        {
+            this.top = null;
+        }
+        public void push(int x)
+        {
+            Node temp = new Node();
+
+            if (temp == null)
+            {
+                Console.Write("\nHeap Overflow");
+                return;
+            }
+            temp.data = x;
+            temp.link = top;
+            top = temp;
+        }
+        public bool isEmpty() { return top == null; }
+        public int peek()
+        {
+            if (!isEmpty())
+            {
+                return top.data;
+            }
+            else
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+        }
+        public void pop()
+        {
+            if (top == null)
+            {
+                Console.Write("\nStack Underflow");
+                return;
+            }
+            top = (top).link;
+        }
+        public void display()
+        {
+            if (top == null)
+            {
+                Console.Write("\nStack Underflow");
+                return;
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data);
+                    temp = temp.link;
+                    if (temp != null)
+                        Console.Write(" -> ");
+                }
+            }
+        }
+    }
+}
