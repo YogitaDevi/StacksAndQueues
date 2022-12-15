037d07ff0722e468150055c1d1af6254a07f2113
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 21:28:08 +0530

    [yogita] create stacks

---
+++ UC1_CreateStacks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using UC1_CreateStacks;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+    
+        StacksUsingLinkedList obj = new StacksUsingLinkedList();
+    
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            obj.display();
+            Console.Write("\nTop of the stacks {0}\n", obj.peek());       
+    }
+}
