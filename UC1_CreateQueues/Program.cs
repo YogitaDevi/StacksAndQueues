d90cbd089c5da6cf9c6e224a2c07da4b45aa6979
YogitaDevi <yogita.gry@gmail.com>
2022-12-15 21:57:24 +0530

    [yogita] create queues

---
+++ UC1_CreateQueues/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+
+public class UC1_CreateQueue
+{
+    public static void Main(String[] args)
+    {
+        Queue qt = new Queue();
+        Console.WriteLine(qt.Count);
+    }
+}
